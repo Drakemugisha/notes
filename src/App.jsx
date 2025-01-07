a7049bde02393aa80de7ca6e8d3853d5f0a10de0
mugisha drake <drakemugisha6@gmail.com>
2025-01-07 16:01:34 +0300

    sigh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import react from "react"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/home"
@@ -18,7 +18,7 @@ function RegisterAndLogout() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route
           path="/"
@@ -33,7 +33,7 @@ function App() {
         <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
